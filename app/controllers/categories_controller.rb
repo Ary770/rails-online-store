badf78eb30d851918cae64a6244dfc0e006c9325
Ary Baldioceda <abaldioceda@gmail.com>
2018-02-14 20:16:58 +0000

    worked on categories controller, index and show views and header

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,31 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+  
+  def show
+    @category = Category.find_by(id: params[:id]) 
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def category_params
+    
+  end
 end
