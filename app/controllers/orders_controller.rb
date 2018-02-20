9a7ec4b4a7169fd8e01347805baf046e119655a0
Ary Baldioceda <abaldioceda@gmail.com>
2018-02-20 20:07:41 +0000

    incorporated orders index view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   load_and_authorize_resource
   
+  def index
+    @orders = current_user.orders
+  end
+  
   def new
     @cart = current_user.cart
     @order = Order.new
