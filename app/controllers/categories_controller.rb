6bbb2fc629d3877dd0cced2a3229b5bcc4e37f34
github username <github email address>
2018-05-09 14:54:05 -0400

    implemented jquery for items nested under categories in /categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,26 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
-  
+
   def index
     @categories = Category.all
   end
-  
+
   def show
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @category }
+    end
   end
-  
+
   def new
     @category = Category.new
   end
-  
+
   def create
     if @category = Category.find_by(name: params[:category][:name])
       flash[:error] = "#{@category.name} already exists."
-      redirect_to category_path(@category) 
+      redirect_to category_path(@category)
     else
       @category = Category.new(category_params)
       if @category.save
@@ -27,7 +31,7 @@ class CategoriesController < ApplicationController
       end
     end
   end
-  
+
   def update
     if @category.update(category_params)
       redirect_to category_path(@category)
@@ -35,19 +39,19 @@ class CategoriesController < ApplicationController
       redirect_to edit_category(@category)
     end
   end
-  
+
   def destroy
     @category.destroy
     flash[:notice] = "Category deleted."
     redirect_to root_path
   end
-  
+
   private
-  
+
   def category_params
     params.require(:category).permit(:name)
   end
-  
+
   def set_category
     @category = Category.find_by(id: params[:id])
   end
