8d4481015ce1180abbc8a83b5a0e4c81dca007a5
github username <github email address>
2018-05-09 16:19:20 -0400

    working on js on admin/items, moved js category code back to categories/index view

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,17 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-$(function(){
-  $(".btn.btn-link").on("click", function(){
-    var id = $(this).data("id")
-    $.getJSON(`/categories/${id}`, function(category_data){
-      var items = category_data["items"]
-      var itemsList = "";
-
-      items.forEach(function(item) {
-        itemsList+= `<li><a href="/items/${item.id}">${item.name} | $${item.price.toLocaleString()}</a></li>`;
-      });
-      $(`#category-item-${id}`).html(itemsList)
-    });
-  });
-});
