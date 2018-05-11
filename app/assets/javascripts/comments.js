d72fb1bdf2bb094ccb7adee323b3bc60ef0dac96
github username <github email address>
2018-05-11 13:11:05 -0400

    created JS class for comments with prototype methods to show comment on item page

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,19 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+class Comment {
+  constructor(attributes) {
+    this.id = attributes.id;
+    this.text = attributes.text;
+    this.user = attributes.user;
+  }
+
+  userEmail(){
+    return this.user.email;
+  }
+
+  buildHtml(){
+    return `<div class="container"><div class="thumbnail"><h4>${this.user.email}</h4><p>${this.text}</p></div></div>`;
+  }
+}
