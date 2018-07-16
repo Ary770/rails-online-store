bc6f1a30bcbeeee7dad15d3369975473c1639b7d
github username <github email address>
2018-07-16 15:34:08 -0400

    working on pushing app to heroku

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PriEtz
+module OnlineRailsStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
